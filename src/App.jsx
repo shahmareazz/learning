b669e9867960a50cc95e5d3863a721dc51942019
shahmareazz <136588415+shahmareazz@users.noreply.github.com>
2023-06-28 15:10:46 +0530

    completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
+import { AddBlog } from "./components/Addblog";
+import Dashboard from "./components/Dashboard";
+import Header from "./components/Header";
+import { BrowserRouter, Route, Routes, Switch} from 'react-router-dom';
+
 function App() {
-return(
-<div className="App">
-  Welcome
-</div>
-);
+  
+  return (
+    <div className="App">
+     <Header />
+     <Routes>
+      <Route path="/" element={<Dashboard />}></Route>
+      <Route path="/Addblog" element={<AddBlog />}></Route>
+     </Routes>
+   
+    </div>
+  );
 }
+
 export default App;
